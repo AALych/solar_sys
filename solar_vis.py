481b44e65c9b513060a3a1f9a5e25ea519590acd
FMariaD <fedorova.md@phystech.edu>
2020-11-20 22:57:25 +0300

    Пофиксила размер экрана

--- solar_vis.py
+++ solar_vis.py
@@ -12,10 +12,10 @@ import pygame as pg
 header_font = "Arial-16"
 """Шрифт в заголовке"""
 
-window_width = 800
+window_width = 1000
 """Ширина окна"""
 
-window_height = 800
+window_height = 900
 """Высота окна"""
 
 scale_factor = 1
